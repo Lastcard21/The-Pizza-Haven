acdd0a2463871ad4f82ef6afb4a2c87636e2bc24
Akande Abdulquadir <akandeabdulquadirolamilekan@gmail.com>
2024-09-06 10:42:41 -0700

    i made changes to the nav bar

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// function showSidebar(){
-//     const sidebar = document.querySelector(".sidebar");
-//     sidebar.style.display = "flex";
-// }
+function showSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
 
 
-// function hideSidebar(){
-//     const sidebar = document.querySelector(".sidebar");
-//     sidebar.style.display = "none";
-// }
+function hideSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
 
 ScrollReveal({ 
     reset: true,
@@ -16,5 +16,7 @@ ScrollReveal({
     delay: 400
  });
 
- ScrollReveal().reveal('.welcome-text, .about-text, .order-btn, .div-text h2, .div-text p', { delay: 500, origin: 'left' });
- ScrollReveal().reveal('.about-img', { delay: 500, origin: 'bottom' });
+ ScrollReveal().reveal('.article, .div-text, .about-img img, .hot, .footer-container, .social-icons, .copy, .quality, .delivery, .recipes', { delay: 500, origin: 'left' });
+ ScrollReveal().reveal('.pizza-pic', { delay: 500, origin: 'bottom' });
+
+ ScrollReveal().reveal('.quality, .newsletter, .recipes, ', { delay: 500, origin: 'right' });
