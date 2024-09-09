8e2f9575fd75c5a7df52ee631913abb60eeea4c2
Akande Abdulquadir <akandeabdulquadirolamilekan@gmail.com>
2024-09-09 09:29:31 -0700

    i made changes to the cards in the home page

--- index.js
+++ index.js
@@ -9,21 +9,33 @@ function hideSidebar(){
     sidebar.style.display = "none";
 }
 
-ScrollReveal({ 
-    reset: true,
-    distance: '60px',
-    duration: 2500,
-    delay: 400
- });
+
+
+var numOfCards = document.querySelectorAll('.pizza-pic').length;
+for(var i = 0;i < numOfCards; i++){
+    document.querySelectorAll('.pizza-pic')[i].addEventListener("mouseenter", function(){
+        this.classList.add('bg-pizza');
+    })
+    document.querySelectorAll('.pizza-pic')[i].addEventListener("mouseleave", function(){
+        this.classList.remove('bg-pizza');
+    })
+}
+
+// ScrollReveal({ 
+//     reset: true,
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400
+//  });
 
 
 
 
-// amimation for index
- ScrollReveal().reveal('.article, .div-text, .hot, .footer-container, .social-icons, .copy, .quality, .delivery, .recipes', { delay: 500, origin: 'left' });
- ScrollReveal().reveal('.pizza-pic', { delay: 500, origin: 'bottom' });
+// // amimation for index
+//  ScrollReveal().reveal('.article, .div-text, .hot, .footer-container, .social-icons, .copy, .quality, .delivery, .recipes', { delay: 500, origin: 'left' });
+//  ScrollReveal().reveal('.pizza-pic', { delay: 500, origin: 'bottom' });
 
- ScrollReveal().reveal('.quality, .newsletter, .recipes, .fine', { delay: 500, origin: 'right' });
+//  ScrollReveal().reveal('.quality, .newsletter, .recipes, .fine', { delay: 500, origin: 'right' });
 
 
 
